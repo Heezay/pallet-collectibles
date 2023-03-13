7ffb59be83ab6616d0d13e2098de862ef99ca1af
Daniil Anikin <81761954+Heezay@users.noreply.github.com>
2023-03-14 00:54:05 +0600

    Update benchmarking.rs

--- pallets/pallet-collectibles/src/benchmarking.rs
+++ pallets/pallet-collectibles/src/benchmarking.rs
@@ -23,13 +23,23 @@ benchmarks! {
 		assert_eq!(OwnerOfCollectibles::<T>::get(receiver), vec![unique_id]);
 	}
 
-	set_price {
+	buy_collectible {
+        // Create new collectible 
 		let caller: T::AccountId = whitelisted_caller();
 		let unique_id: [u8; 16] = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 		let collectible_id = CollectiblesModule::<T>::mint(&caller, unique_id, Color::Red).unwrap();
-		let buyer: T::AccountId = whitelisted_caller();
-		let new_price = T::Currency::free_balance(&buyer);
-	  }: set_price(RawOrigin::Signed(caller), collectible_id,  Some(new_price))
+
+        // Set price for new collectible to avoid return Error(NotForSale)
+        let new_price = T::Currency::free_balance(&caller);
+		CollectiblesModule::<T>::set_price(RawOrigin::Signed(caller).into(), collectible_id,  Some(new_price)).unwrap();
+
+        // Create new account and make free balance for it
+		let receiver = account::<T::AccountId>("receiver", 1u32, 2u32);
+        let bid_price = T::Currency::free_balance(&receiver);
+	}: buy_collectible(RawOrigin::Signed(receiver.clone()), collectible_id,  bid_price)
+	verify {
+		assert_eq!(OwnerOfCollectibles::<T>::get(receiver), vec![unique_id]);
+	}
 
 	impl_benchmark_test_suite!(CollectiblesModule, crate::tests::new_test_ext(), crate::tests::Test);
 }
